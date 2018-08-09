4480169f85e5599d0e53dd78465c6484e0519fed
kotroczo.mate <kotroczo.mate@gmail.com>
2018-08-09 15:10:46 +0200

    form render admin.actions.show

--- resources/views/form/footer.blade.php
+++ resources/views/form/footer.blade.php
@@ -1,30 +1,37 @@
 <div class="box-footer">
 
-    {{ csrf_field() }}
+  {{ csrf_field() }}
 
-    <div class="col-md-{{$width['label']}}">
-    </div>
+  <div class="col-md-{{$width['label']}}">
+  </div>
 
-    <div class="col-md-{{$width['field']}}">
+  <div class="col-md-{{$width['field']}}">
 
-        @if(in_array('submit', $buttons))
-        <div class="btn-group pull-right">
-            <button class="btn btn-primary">{{ trans('admin.submit') }}</button>
-        </div>
+    @if(in_array('submit', $buttons))
+      <div class="btn-group pull-right">
+        <button class="btn btn-primary">{{ trans('admin.submit') }}</button>
+      </div>
 
+      <label class="pull-right" style="margin: 5px 10px 0 0;">
+        <input type="checkbox" class="after-submit" name="after-save" value="1">
+        &nbsp;
+        {{ trans('admin.continue_editing') }}
+      </label>
+
+      @if(config('admin.actions.show'))
         <label class="pull-right" style="margin: 5px 10px 0 0;">
-            <input type="checkbox" class="after-submit" name="after-save" value="1"> {{ trans('admin.continue_editing') }}
+          <input type="checkbox" class="after-submit" name="after-save" value="2">
+          &nbsp;
+          {{ trans('admin.view') }}
         </label>
+      @endif
 
-        <label class="pull-right" style="margin: 5px 10px 0 0;">
-            <input type="checkbox" class="after-submit" name="after-save" value="2"> {{ trans('admin.view') }}
-        </label>
-        @endif
+    @endif
 
-        @if(in_array('reset', $buttons))
-        <div class="btn-group pull-left">
-            <button type="reset" class="btn btn-warning">{{ trans('admin.reset') }}</button>
-        </div>
-        @endif
-    </div>
+    @if(in_array('reset', $buttons))
+      <div class="btn-group pull-left">
+        <button type="reset" class="btn btn-warning">{{ trans('admin.reset') }}</button>
+      </div>
+    @endif
+  </div>
 </div>
