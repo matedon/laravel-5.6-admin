0bced8465c26b08678da2f16b25eceececd66e8f
kotroczo.mate <kotroczo.mate@gmail.com>
2018-08-09 14:24:55 +0200

    use admin_translate for the menu tree too

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,27 +1,28 @@
 @if(Admin::user()->visible($item['roles']))
-    @if(!isset($item['children']))
-        <li>
-            @if(url()->isValidUrl($item['uri']))
-                <a href="{{ $item['uri'] }}" target="_blank">
-            @else
-                 <a href="{{ admin_base_path($item['uri']) }}">
-            @endif
-                <i class="fa {{$item['icon']}}"></i>
-                <span>{{$item['title']}}</span>
+  @if(!isset($item['children']))
+    <li>
+      @if(url()->isValidUrl($item['uri']))
+        <a href="{{ $item['uri'] }}" target="_blank">
+          @else
+            <a href="{{ admin_base_path($item['uri']) }}">
+              @endif
+              <i class="fa fa-lg fa-fw {{$item['icon']}}"></i>
+              &nbsp;
+              <span>{{admin_translate('menu', $item['title'], $item['title'])}}</span>
             </a>
-        </li>
-    @else
-        <li class="treeview">
-            <a href="#">
-                <i class="fa {{$item['icon']}}"></i>
-                <span>{{$item['title']}}</span>
-                <i class="fa fa-angle-left pull-right"></i>
-            </a>
-            <ul class="treeview-menu">
-                @foreach($item['children'] as $item)
-                    @include('admin::partials.menu', $item)
-                @endforeach
-            </ul>
-        </li>
-    @endif
+    </li>
+  @else
+    <li class="treeview">
+      <a href="#">
+        <i class="fa {{$item['icon']}}"></i>
+        <span>{{$item['title']}}</span>
+        <i class="fa fa-angle-left pull-right"></i>
+      </a>
+      <ul class="treeview-menu">
+        @foreach($item['children'] as $item)
+          @include('admin::partials.menu', $item)
+        @endforeach
+      </ul>
+    </li>
+  @endif
 @endif
