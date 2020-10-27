07340358d3100fe6b3ff3fcc61bbb112b4f6c437
kotroczo.mate <kotroczo.mate@gmail.com>
2020-10-27 12:20:44 +0100

    mod: image attributes height width max-height max-width

--- src/Grid/Displayers/Image.php
+++ src/Grid/Displayers/Image.php
@@ -7,22 +7,55 @@ use Illuminate\Support\Facades\Storage;
 
 class Image extends AbstractDisplayer
 {
-    public function display($server = '', $width = 200, $height = 200)
-    {
+    public function display(
+        $server = '',
+        $maxWidth = 200,
+        $maxHeight = 200,
+        $width = null,
+        $height = null,
+        $class = 'img img-thumbnail'
+    ) {
         if ($this->value instanceof Arrayable) {
             $this->value = $this->value->toArray();
         }
 
-        return collect((array) $this->value)->filter()->map(function ($path) use ($server, $width, $height) {
+        return collect((array)$this->value)->filter()->map(function ($path) use (
+            $server,
+            $maxWidth,
+            $maxHeight,
+            $width,
+            $height,
+            $class
+        ) {
             if (url()->isValidUrl($path)) {
                 $src = $path;
             } elseif ($server) {
-                $src = $server.$path;
+                $src = $server . $path;
             } else {
                 $src = Storage::disk(config('admin.upload.disk'))->url($path);
             }
+            $styleArgs = [];
+            if (!is_null($maxWidth)) {
+                $styleArgs['max-width'] = $maxWidth . 'px';
+            }
+            if (!is_null($maxHeight)) {
+                $styleArgs['max-height'] = $maxHeight . 'px';
+            }
+            if (!is_null($width)) {
+                $styleArgs['width'] = $width . 'px';
+            }
+            if (!is_null($height)) {
+                $styleArgs['height'] = $height . 'px';
+            }
+            $styles = [];
+            foreach ($styleArgs as $key => $val) {
+                $styles [] = "$key:$val";
+            }
+            if (!empty($styles)) {
+                $style = 'style="' . implode($styles, '; ') . '"';
+            }
 
-            return "<img src='$src' style='max-width:{$width}px;max-height:{$height}px' class='img img-thumbnail' />";
+            return "<img src='$src' $style class='{$class}' />";
         })->implode('&nbsp;');
     }
 }
